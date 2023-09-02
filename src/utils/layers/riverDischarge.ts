c8eb7f234d31705c423368db63c6d81120f370c3
Jonny Dawe <59748759+JonnyDawe@users.noreply.github.com>
2023-09-02 18:09:55 +0100

    add animated CIM line to show flow

--- src/utils/layers/riverDischarge.ts
+++ src/utils/layers/riverDischarge.ts
@@ -1,4 +1,6 @@
 import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer.js";
+import CIMSymbol from "@arcgis/core/symbols/CIMSymbol.js";
 
 export function getRiverDischargeLayer() {
     const geojsonlayer = new GeoJSONLayer({
@@ -14,6 +16,59 @@ export function getRiverDischargeLayer() {
             }
         } as any
     });
+
+    //Animation logic - basic as anything!
+    let offset = 0;
+    window.setInterval(() => {
+        offset++;
+        geojsonlayer.renderer = new SimpleRenderer({
+            symbol: new CIMSymbol({
+                data: {
+                    type: "CIMSymbolReference",
+                    symbol: {
+                        type: "CIMLineSymbol",
+                        symbolLayers: [
+                            {
+                                // white dashed layer at center of the line
+                                type: "CIMSolidStroke",
+                                effects: [
+                                    {
+                                        type: "CIMGeometricEffectDashes",
+                                        offsetAlongLine: offset,
+                                        dashTemplate: [3, 2, 3, 2], // width of dashes and spacing between the dashes
+                                        lineDashEnding: "NoConstraint"
+                                    }
+                                ],
+                                enable: true, // must be set to true in order for the symbol layer to be visible
+                                capStyle: "Butt",
+                                joinStyle: "Round",
+                                width: 2,
+                                color: [115, 63, 46, 255]
+                            },
+                            {
+                                // lighter green line layer that surrounds the dashes
+                                type: "CIMSolidStroke",
+                                enable: true,
+                                capStyle: "Butt",
+                                joinStyle: "Round",
+                                width: 3,
+                                color: [170, 93, 68, 255]
+                            },
+                            {
+                                // darker green outline around the line symbol
+                                type: "CIMSolidStroke",
+                                enable: true,
+                                capStyle: "Butt",
+                                joinStyle: "Round",
+                                width: 6,
+                                color: [115, 63, 46, 255]
+                            }
+                        ]
+                    }
+                }
+            })
+        });
+    }, 100);
 
     return geojsonlayer;
 }
