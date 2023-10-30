a63b68ef8ebcb41e366f60e66f0fc72be67842ad
Jonny Dawe <59748759+JonnyDawe@users.noreply.github.com>
2023-10-30 00:18:29 +0000

    test rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
+import { UserConfig, defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,16 @@ export default defineConfig({
         react({
             plugins: [["@swc/plugin-emotion", {}]],
             jsxImportSource: "@emotion/react"
-        })
-    ]
+        }),
+        vercel()
+    ],
+    vercel: {
+        additionalEndpoints: [
+            {
+                source: "src/endpoints/og.tsx",
+                destination: `og`,
+                addRoute: true
+            }
+        ]
+    }
 });
