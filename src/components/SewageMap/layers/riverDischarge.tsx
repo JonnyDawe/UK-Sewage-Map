10c8bb2744d2ed1d5ae160d3a7c526beaa7549f5
Alex Lipp <alex@lipp.org.uk>
2025-01-18 18:42:24 +0000

    Load in downstream reaches

--- src/components/SewageMap/layers/riverDischarge.tsx
+++ src/components/SewageMap/layers/riverDischarge.tsx
@@ -69,24 +69,41 @@ export function RiverDischargeGeoJsonLayer() {
             })
         });
 
-        requestAnimationFrame((timestamp) => animate(layer, timestamp)); // Call animate again on the next frame
+        // requestAnimationFrame((timestamp) => animate(layer, timestamp)); // Call animate again on the next frame
     }
 
+    const urls = [
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/thames/thames_now_incl_48hrs.geojson", // Thames Water
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/anglian/anglian_now_incl_48hrs.geojson", // Anglian Water
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/united/united_now_incl_48hrs.geojson", // United Utilities
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/southern/southern_now_incl_48hrs.geojson", // Southern Water
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/northumbrian/northumbrian_now_incl_48hrs.geojson", // Northumbrian Water
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/severntrent/severntrent_now_incl_48hrs.geojson", // Severn Trent Water
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/wessex/wessex_now_incl_48hrs.geojson", // Wessex Water
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/yorkshire/yorkshire_now_incl_48hrs.geojson", // Yorkshire Water
+        "https://d1kmd884co9q6x.cloudfront.net/downstream_impact/southwest/southwest_now_incl_48hrs.geojson" // South West Water
+    ];
+
     return (
-        <ArcGeoJSONLayer
-            url="https://d1kmd884co9q6x.cloudfront.net/now/now.geojson"
-            copyright="Sewage Map"
-            renderer={
-                new SimpleRenderer({
-                    symbol: new LineSymbol({
-                        color: "#733f2e",
-                        width: "6px"
-                    })
-                })
-            }
-            onLayerCreated={(layer) => {
-                requestAnimationFrame((timestamp) => animate(layer, timestamp));
-            }}
-        />
+        <>
+            {urls.map((url, index) => (
+                <ArcGeoJSONLayer
+                    key={index}
+                    url={url}
+                    copyright="Sewage Map"
+                    renderer={
+                        new SimpleRenderer({
+                            symbol: new LineSymbol({
+                                color: "#733f2e",
+                                width: "6px"
+                            })
+                        })
+                    }
+                    onLayerCreated={(layer) => {
+                        requestAnimationFrame((timestamp) => animate(layer, timestamp));
+                    }}
+                />
+            ))}
+        </>
     );
 }
