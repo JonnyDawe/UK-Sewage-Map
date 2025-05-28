fc9300ec270ba7ae3baf5c9b02e071e50593006c
Jonny Dawe <59748759+JonnyDawe@users.noreply.github.com>
2025-05-28 21:26:37 +0100

    Update hosted image URLs

--- src/constants/hostedImages.ts
+++ src/constants/hostedImages.ts
@@ -1,10 +1,10 @@
 export const POOICON =
-  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_192,h_192/v1737740903/poo_oowyfs.png';
+  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_160,h_160/v1737740903/poo_oowyfs.png';
 export const ERRORICON =
-  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_192,h_192/v1737740903/error-warning-fill_s1qdta.png';
+  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_160,h_160/v1737740903/error-warning-fill_s1qdta.png';
 export const GREENTICKICON =
-  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_192,h_192/v1737740903/shield-check-fill-map-icon_ghb7w1.png';
+  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_160,h_160/v1737740903/shield-check-fill-map-icon_ghb7w1.png';
 export const GREENTICKICONSMALL =
-  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_192,h_192,c_pad,b_transparent,bo_25px_solid_transparent/v1737740903/shield-check-fill-map-icon_ghb7w1.png';
+  'https://res.cloudinary.com/dqdlrq04x/image/upload/v1748463854/not-discharging-map-symbol_jeekiq.png';
 export const UNKNOWNICON =
-  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_192,h_192/v1737740903/unkown-source_qio3g7.png';
+  'https://res.cloudinary.com/dqdlrq04x/image/upload/w_160,h_160/v1737740903/unkown-source_qio3g7.png';
